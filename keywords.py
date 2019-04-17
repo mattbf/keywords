307c59b671b0da69af3d84373b1a600da3c59ddc
mattbf <matthewbf8@gmail.com>
2019-04-17 10:20:37 -0600

    Tasks 3-6 complete: Create a dataframe, rename columns, and add campign column

--- keywords.py
+++ keywords.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+import pandas as pd
 
 # Define words list, product list, and empty keyword list
 words = ['buy', 'price', 'discount', 'promotion', 'promo', 'shop']
@@ -11,4 +12,8 @@ for product in products:
     for word in words:
         keywords_list.append([product, product + ' ' + word])
         keywords_list.append([product, word + ' ' + product])
-pprint(keywords_list)
+# Make the list a DataFrame and rename columns
+keywords_df = pd.DataFrame.from_records(keywords_list, columns=['Ad Group', 'Keyword'])
+
+
+pprint(keywords_df)
