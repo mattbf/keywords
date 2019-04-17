67a279bbd8179a19bec9c4f236dbf48b6586549c
mattbf <matthewbf8@gmail.com>
2019-04-17 10:03:54 -0600

    Task 2 complete: appenede words to both ends of products and created list

--- keywords.py
+++ keywords.py
@@ -1,5 +1,14 @@
-# List of words to pair with products
-words = ['buy', 'price', 'discount', 'promotion', 'promo', 'shop']
+from pprint import pprint
 
-# Print list of words
-print(words)
+# Define words list, product list, and empty keyword list
+words = ['buy', 'price', 'discount', 'promotion', 'promo', 'shop']
+products = ['sofas', 'convertible sofas', 'love seats', 'recliners', 'sofa beds']
+keywords_list = []
+
+# loop through lists to append words and products together
+
+for product in products:
+    for word in words:
+        keywords_list.append([product, product + ' ' + word])
+        keywords_list.append([product, word + ' ' + product])
+pprint(keywords_list)
